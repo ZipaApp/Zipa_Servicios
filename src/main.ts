fbc32709f8904b32c255f2a41c085c6b5c9a1f03
Fabian Torres <fatorresra@unal.edu.co>
2025-10-02 01:23:21 -0500

    Enable CORS for all routes

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); // Opcional: todas las rutas empiezan con /api
+  
+  app.enableCors({
+    origin: '*', // Permitir todas las fuentes
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+  });
   await app.listen(3000);
   console.log('Backend corriendo en http://localhost:3000');
 }
